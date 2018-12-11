cf2d4ad72c5c53d2bf536992f6c1afa1ba5a61e3
Sean Zhang <seanzhang@tongji.edu.cn>
2018-12-12 02:26:31 +0800

    Initial upload

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # RemoveBackground
+
+For those who found a really nice free icon/illustration/etc., but sadly realized it's not RGBA. This quick and dirty script is for you. Below are some examples (I added grid in the background to show the transparent parts):
+
+![](demo1.png)
+
+![](demo2.png)
+
+![](demo3.png)
+
+## Usage
+
+```shell
+usage: python3 RemoveBackground.py [-h] [-c C C C] [-e E] [-g G] image
+
+(Hopefully) removes single-colored background from images.
+
+positional arguments:
+  image
+
+optional arguments:
+  -h, --help  show this help message and exit
+  -c C C C    RGB value of the background. Defaults to white.
+  -e E        If you want to crank up the alpha value, set to something
+              greater than 1.0.
+  -g G        If the algorithm is eating up the edges, set a value higher than
+              1.0.
+```
+
+## How does it work?
+
+Basically, if we want to get the original RGBA image, we're solving this equation:
+
+```Alpha * OriginalRGB + (1 - Alpha) * BackgroundRGB = ImageRGB
+Alpha * OriginalRGB + (1 - Alpha) * BackgroundRGB = ImageRGB
+```
+
+We have to estimate one to get the other. Here we try to estimate Alpha. Do some tweaking and we get:
+
+```text
+OriginalRGB = ImageRGB - BackgroundRGB / Alpha + BackgroundRGB
+```
+
+One way often used by designers when it comes to removing black background, is to use the brightness of the image as alpha. Here we learn from this approach. The first step is to transform our problem (arbitrary background color) to a black background problem. We do this by simply subtracting background from foreground and taking absolute value:
+
+```
+Alpha = BlackBackgroundImage = abs(Image - Background)
+```
+
+However, this Alpha is often very dark, and makes the whole image somewhat transparent. One way to do this is to crank up the value by multiplying some constant. However, in this process we often lose some fine anti-aliased edges. One obvious is to protect the edges by masking them from the multiplication. Gradients work pretty well for this purpose. 
+
+```
+EdgesMask = HorizontalGradient(Alpha) + VerticalGradient(Alpha)
+EnhancerImage = (1 - EdgesMask) * e  //This is the e in the script parameters!
+Alpha = Alpha .* EnhancerImage  //Elementwise multiplication.
+```
+
+One problem with this is that the gradient mask sometimes mask places that should be enhanced, therefore leaving tiny cracks with low alpha. We get rid of these by applying a 3 * 3 max filter and a 3 * 3 min filter. 
+
+One final problem is that our EnhancerImage might not be enough. When finally solving for the original colors (OriginalRGB), we sometimes see values greater than 255 or less than 0. We measure can measure this error by calculating the sum of values less than 0 (absolute value, of course), and those greater than 255. We loop repeatedly to enhance Alpha until this error reaches 0. 
+
+Voila! You get your clean RGBA image.
+
+## Sidenote
+
+This is a very naive method to unmix colors. It is prone to noise and does not work very well with photos. For a "proper" way of unmixing colors, see this amazing work published by Disney Research Zürich and  ETH Zürich: [Interactive High-Quality Green-Screen Keying via Color Unmixing](https://s3-us-west-1.amazonaws.com/disneyresearch/wp-content/uploads/20160816162952/Interactive-High-Quality-Green-Screen-Keying-via-Color-Unmixing-Paper.pdf)
---
+++ RemoveBackground.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import os.path
+from math import tanh
+import cv2
+import numpy as np
+from scipy.signal import convolve2d
+
+parser = argparse.ArgumentParser(description='(Hopefully) removes single-colored background from images.')
+parser.add_argument('-c', nargs=3, default=[255, 255, 255], help="RGB value of the background. Defaults to white.")
+parser.add_argument('-e', default=1.0, type=float, help="If you want to crank up the alpha value, set to something greater than 1.0.")
+parser.add_argument('-g', default=1.0, type=float, help="If the algorithm is eating up the edges, set a value higher than 1.0.")
+parser.add_argument('image')
+
+args = parser.parse_args(sys.argv[1:])
+
+original = cv2.imread(args.image)
+background = np.zeros_like(original)
+background[..., 2] = int(args.c[0])
+background[..., 1] = int(args.c[1])
+background[..., 0] = int(args.c[2])
+
+difference = np.maximum(original, background) - np.minimum(original, background)
+
+alpha = np.maximum(difference[..., 0], difference[..., 1])
+alpha = np.maximum(alpha, difference[..., 2])
+alpha = alpha.astype(np.float64) / 255.0
+
+# Now we use gradient to extract all edges in the image
+gradient1 = np.abs(np.gradient(alpha)[0])
+gradient2 = np.abs(np.gradient(alpha)[1])
+gradient = np.maximum(gradient1, gradient2)
+gradient *= args.g / np.max(gradient)
+gradient[gradient > 1] = 1.0
+
+enhance = np.abs(gradient - np.ones_like(gradient))
+enhance *= args.e
+enhance[enhance < 1.0] = 1.0
+
+alpha = alpha * enhance
+alpha[alpha > 1.0] = 1.0
+alpha_old = alpha.copy()
+
+alpha = cv2.dilate(alpha, kernel=np.ones((5,5),np.uint8)).astype(np.uint8)
+alpha = cv2.erode(alpha, kernel=np.ones((5,5),np.uint8)).astype(np.uint8)
+
+factor = tanh(args.e - 1)
+alpha = factor * alpha + (1 - factor) * alpha_old
+
+# now that we have estimated alpha, let's solve for the original colors
+# we use the formula c_orig = (c_foregroud - (1 - alpha) * c_background) / alpha.
+inverted_alpha = np.ones_like(alpha) - alpha
+
+alpha_bgr = np.stack([alpha]*3, axis=2)
+# print(np.min(alpha_bgr))
+inverted_alpha_bgr = np.stack([inverted_alpha]*3, axis=2)
+solved = np.divide(original - inverted_alpha_bgr * background + 1e-20, alpha_bgr + 1e-20)
+
+error = abs(np.sum(solved[solved < 0])) + np.sum(solved[solved > 255])
+
+while error > 10:#  and abs(last_error - error) > 1e-10:
+    try:
+        print(error, np.sum(alpha))
+        mask1 = np.bitwise_or(solved[...,0] < 0, solved[...,1] < 0, solved[...,2] < 0)
+        mask2 = np.bitwise_or(solved[...,0] > 255, solved[...,1] > 255, solved[...,2] > 255)
+        mask = np.bitwise_or(mask1, mask2)
+        alpha *= 1.05
+        alpha[alpha > 1.0] = 1.0
+        alpha_bgr = np.stack([alpha]*3, axis=2)
+        inverted_alpha = np.ones_like(alpha) - alpha
+        inverted_alpha_bgr = np.stack([inverted_alpha]*3, axis=2)
+        solved = np.divide(original - inverted_alpha_bgr * background + 1e-20, alpha_bgr + 1e-20)
+        last_error = error
+        error = abs(np.sum(solved[solved < 0])) + np.sum(solved[solved > 255])
+    except KeyboardInterrupt:
+        cv2.imwrite(os.path.splitext(args.image)[0] + ".alpha.png", alpha * 255)
+        sys.exit()
+
+
+solved = solved.astype(np.uint8)
+
+
+alpha = (alpha * 255).astype(np.uint8)
+blue, green, red = cv2.split(solved)
+final = cv2.merge([blue, green, red, alpha])
+
+enhance_r = enhance / np.max(enhance) * 255
+enhance_g = (1 - enhance / np.max(enhance)) * 255
+
+
+cv2.imwrite(os.path.splitext(args.image)[0] + ".processed.png", final)
+# cv2.imwrite(os.path.splitext(args.image)[0] + ".solved.png", solved)
+# cv2.imwrite(os.path.splitext(args.image)[0] + ".alpha.png", alpha)
+# cv2.imwrite(os.path.splitext(args.image)[0] + ".edges.png", gradient * 255)
+# cv2.imwrite(os.path.splitext(args.image)[0] + ".enhance.png", cv2.merge([enhance_g, enhance_g, enhance_r]))
